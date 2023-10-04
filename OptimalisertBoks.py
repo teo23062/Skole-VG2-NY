235aedd26b91426782ad1175a52d12624f77d703
Todor <144096670+teo23062@users.noreply.github.com>
2023-10-04 12:31:38 +0200

    Redigerte oppgave
    Redigerte oppgave

--- OptimalisertBoks.py
+++ OptimalisertBoks.py
@@ -5,16 +5,21 @@ Hoyde = 1
 Optimalisering = False
 MaksHoyde = 0
 NoverendeHoyde = 0
+mellomhoyde = 0
 
 while not Optimalisering:
-    NoverendeHoyde = (Bredde - (Hoyde * 2)) * (Lengde - (Hoyde * 2)) * Hoyde / 1000000
 
-    if NoverendeHoyde > MaksHoyde:
-        MaksHoyde = NoverendeHoyde
+    NoverendeHoyde = ((Bredde - (Hoyde * 2)) * (Lengde - (Hoyde * 2))) * Hoyde / 1000000
+
+    if NoverendeHoyde > mellomhoyde:
+        mellomhoyde = NoverendeHoyde
 
     Hoyde += 0.1
 
-    if NoverendeHoyde <= MaksHoyde:
+    if mellomhoyde > MaksHoyde:
+        MaksHoyde = mellomhoyde
+
+    if NoverendeHoyde < MaksHoyde:
         Optimalisering = True
 
 print("Maksimal høyde for optimalt volum er: " + str(MaksHoyde))
