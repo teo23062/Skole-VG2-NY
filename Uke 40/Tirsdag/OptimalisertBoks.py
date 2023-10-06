2ab8fba1fe08773c38fb9dfd302cc2b8f440a128
Todor <144096670+teo23062@users.noreply.github.com>
2023-10-06 08:42:20 +0200

    Gjorde ferdig OptimalA4Boks
    Gjorde ferdig optimal a4 boks oppgaven

--- Uke 40/Tirsdag/OptimalisertBoks.py
+++ Uke 40/Tirsdag/OptimalisertBoks.py
@@ -1,2 +1,22 @@
-#Hello
-#Simon er kul
+Lengde = 297
+Bredde = 210
+
+hoyde = 0
+
+MaksHoyde = 0
+NoverendeHoyde = 0
+
+while NoverendeHoyde == MaksHoyde:
+    vLengde = Lengde - (hoyde*2)
+    vBredde = Bredde - (hoyde*2)
+
+    NoverendeHoyde = (vBredde * vLengde * hoyde) / 1000000
+
+    print("Utregnet volum: " + str(NoverendeHoyde))
+
+    if NoverendeHoyde > MaksHoyde:
+        MaksHoyde = NoverendeHoyde
+    
+    hoyde += 1
+
+print("Maksimal høyde for optimalt volum er: " + str(MaksHoyde))
